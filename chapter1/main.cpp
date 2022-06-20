8b39cdd4391c4efb7f2417b8d73988f6cbcf3c27
egocentryczny <galazka.milosz@gmail.com>
2022-06-20 22:50:23 +0200

    chap1

---
+++ chapter1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+
+    //program 1
+
+
+    // std::cout << "Enter an integer: ";
+    // int num{};
+    // std::cin >> num;
+
+    // std::cout << "Double is: " << num * 2 << std::endl;
+    // std::cout << "Triple is: " << num * 3 << std::endl;
+
+    // return 0;
+
+    //program 2
+
+    std::cout << "Enter the first number: ";
+    int num1{};
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    int num2{};
+    std::cin >> num2;
+
+    std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
+    std::cout << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
+
+
+    return 0;
+}
